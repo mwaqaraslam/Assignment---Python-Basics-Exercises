18deabfec68f6c46838810987f485918a23aebf8
Muhammad Waqar Aslam - 7433 <waqar.aslam@bankalfalah.com>
2024-08-08 10:33:07 +0500

    first commit

---
+++ basics.py
@@ -0,0 +1,63 @@
+message = "Assignment of Python Basics Exercises"
+print(message)
+message = "variable related assignment"
+print(message)
+message = "Its 3rd assignment"
+print(message)
+name = "Eric"
+print(f"Hello {name}, would you like to learn some Python today?")
+my_name = "Muhammad waqar aslam"
+print(my_name.lower())
+print(my_name.upper())
+print(my_name.title())
+quote = "With faith, discipline and selfless devotion to duty, there is nothing worthwhile that you cannot achieve."
+author = "Quaid-e-Azam"
+print(f"{author} once said,\"{quote}\"")
+famous_person = "Allama Muhammad Iqbal"
+quote_1 = "Words, without power, is mere philosophy."
+message = f"{famous_person} once said,\"{quote_1}\""
+print(message)
+name_1 = "   UMT Lahore    "
+print(name_1)
+print(name_1.lstrip())
+print(name_1.rstrip())
+print(name_1.strip())
+print("UMT\nLahore")
+print("UMT\tLahore")
+x=5
+y=10
+z=15
+print(x+y+z)
+a=10
+b=25
+print(f"before_swap: a={a}, b={b}")
+a,b=b,a
+print(f"after_swap: a={a}, b={b}")
+fav_color = "Green"
+print(fav_color)
+new_color = fav_color
+print(new_color)
+pet_name = "Buddy"
+print(pet_name)
+pet_name = "Max"
+print(pet_name)
+name_error = "Sunshine"
+print(name_error)
+score = 100
+print(score)
+score = 150
+print(score)
+city: str="Lahore"
+print(city)
+text = "python programming"
+print(text.title())
+text_1="certificate of Cloud native"
+print(text_1.upper())
+print(text_1.lower())
+temperature = 25
+print(f"The current temperature is {temperature} degrees.")
+poem = """A solitary soul, a quiet mind,
+In nature's embrace, peace I find.
+With every breath, a deeper sense,
+Of inner calm, a sweet defense"""
+print(poem)
